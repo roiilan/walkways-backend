bb5213efc50d088dc7bffffddd3ddf9f5987721d
roi.ilan <roiilan23@gmail.com>
2020-03-26 19:50:14 +0200

    atlas mongo

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,6 @@
 module.exports = {
-    "dbURL": "mongodb://localhost:27017",
+    // "dbURL": "mongodb://localhost:27017",
     // "dbURL": "mongodb://tamir:12345@cluster0-shard-00-00-j35sr.mongodb.net:27017,cluster0-shard-00-01-j35sr.mongodb.net:27017,cluster0-shard-00-02-j35sr.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+    "dbURL": "mongodb://roiilan:3465roi@roiilan-shard-00-00-qysrp.mongodb.net:27017,roiilan-shard-00-01-qysrp.mongodb.net:27017,roiilan-shard-00-02-qysrp.mongodb.net:27017/test?ssl=true&replicaSet=roiilan-shard-0&authSource=admin&retryWrites=true&w=majority"
 
 }
